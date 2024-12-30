afa69888e9852ac0555a8ec33d26167f26978eca
Shayan Sharifi <shmanager52@gmail.com>
2024-12-31 00:36:02 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ from pyrogram import Client, filters
 from pyrogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 
-# اطلاعات 
+#  app اطلاعات 
 API_TOKEN = "7575235119:AAGFR15l6OFkOq_8S05WXfTvoRuu4YCf0vQ"  # جایگزین کنید با کلید API خود
 APP_ID = 25709855  # App ID تلگرام
 APP_HASH = "740efc27f273ac589176b85853ef8088"  # App Hash تلگرام
